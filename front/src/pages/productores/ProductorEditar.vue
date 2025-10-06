56192d145702d9dbb973ea38d9de05255c75c45b
adimerpaul <adimer101@gmail.com>
2025-10-06 01:53:02 -0400

    Creado tab de productores

--- front/src/pages/productores/ProductorEditar.vue
+++ front/src/pages/productores/ProductorEditar.vue
@@ -8,40 +8,72 @@
     </div>
 
     <q-card flat bordered>
-      <q-inner-loading :showing="loading">
-        <q-spinner />
-      </q-inner-loading>
+      <q-tabs v-model="tab" class="text-primary" align="left" dense>
+        <q-tab name="general" icon="badge" label="1) Información general" no-caps />
+        <q-tab name="certs" icon="verified" label="2) Certificaciones" no-caps />
+        <q-tab name="apiarios" icon="hive" label="3)Apiarios" no-caps />
+         <q-tab name="mapa" icon="map" label="4) Mapa" no-caps />
+      </q-tabs>
+      <q-separator />
 
-      <q-card-section v-if="!loading && productor">
-        <ProductorForm
-          :productor="productor"
-          banner-msg="Actualiza los datos y guarda los cambios"
-          @saved="onSaved"
-          @cancel="$router.back()"
-        />
-      </q-card-section>
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="general" class="q-pa-none">
+          <q-card-section v-if="!loading && productor">
+            <ProductorForm
+              :productor="productor"
+              banner-msg="Actualiza los datos y guarda los cambios"
+              @saved="onSaved"
+              @cancel="$router.back()"
+            />
+          </q-card-section>
+        </q-tab-panel>
 
-      <q-card-section v-if="!loading && !productor" class="text-grey">
-        No se encontró el productor.
-      </q-card-section>
+        <q-tab-panel name="certs" class="q-pa-none">
+          <ProductorCertificaciones :productor="productor" />
+        </q-tab-panel>
+
+        <q-tab-panel name="apiarios" class="q-pa-none">
+          <ProductorApiarios :productor="productor" />
+        </q-tab-panel>
+        <q-tab-panel name="mapa" class="q-pa-none">
+          <ProductorMapa :productor="productor" />
+        </q-tab-panel>
+      </q-tab-panels>
     </q-card>
   </q-page>
 </template>
 
 <script>
-
-import ProductorForm from "pages/productores/ProductorForm.vue";
+import ProductorForm from 'pages/productores/ProductorForm.vue'
+import ProductorCertificaciones from "pages/productores/tabs/ProductorCertificaciones.vue";
+import ProductorApiarios from "pages/productores/tabs/ProductorApiarios.vue";
+import ProductorMapa from "pages/productores/tabs/ProductorMapa.vue";
 
 export default {
   name: 'ProductorEditar',
-  components: {ProductorForm},
+  components: {ProductorMapa, ProductorApiarios, ProductorCertificaciones, ProductorForm },
   data () {
     return {
+      tab: 'general',
       loading: false,
-      productor: null
+      productor: null,
+      certsLoading: false,
+      apiLoading: false,
+      certsLoaded: false,
+      apiLoaded: false,
+      certs: [],
+      apiarios: []
     }
   },
-  mounted () { this.fetchProductor() },
+  // watch: {
+  //   tab (t) {
+  //     if (t === 'certs' && !this.certsLoaded) this.fetchCerts()
+  //     if (t === 'apiarios' && !this.apiLoaded) this.fetchApiarios()
+  //   }
+  // },
+  mounted () {
+    this.fetchProductor()
+  },
   methods: {
     async fetchProductor () {
       this.loading = true
@@ -57,11 +89,8 @@ export default {
       }
     },
     onSaved (saved) {
-      // Si quieres permanecer en la misma vista, refresca los datos:
       this.productor = saved
       this.$alert?.success?.('Cambios guardados')
-      // O redirige:
-      // this.$router.push({ name: 'productores.index' })
     }
   }
 }
---
+++ front/src/pages/productores/tabs/ProductorCertificaciones.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-card-section >
+    <pre>{{productor}}</pre>
+  </q-card-section>
+</template>
+<script>
+export default {
+  name: 'ProductorCertificaciones',
+  props: {
+    productor: {
+      type: Object,
+      required: true
+    },
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
