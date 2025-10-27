21f7036d667254f555194707171d2b61bcb66b77
adimerpaul <adimer101@gmail.com>
2025-10-27 06:35:41 -0400

    Creado campos para slam

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -1,10 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+<!--    <img-->
+<!--      alt="Quasar logo"-->
+<!--      src="~assets/quasar-logo-vertical.svg"-->
+<!--      style="width: 200px; height: 200px"-->
+<!--    >-->
+<!--    descargar de public EbaAppV1.0.1.apk-->
+    <a href="EbaAppV1.0.1.apk" download>
+      <q-btn color="primary" label="Descargar EbaApp" />
+    </a>
   </q-page>
 </template>
 
