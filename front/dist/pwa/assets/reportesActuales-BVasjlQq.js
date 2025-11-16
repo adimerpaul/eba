f4be187aa80ca8f0c826b973d9a690b9d944ee93
AFLopezG <35200781+AFLopezG@users.noreply.github.com>
2025-11-15 22:00:25 -0400

    mod

---
+++ front/dist/pwa/assets/reportesActuales-BVasjlQq.js
@@ -0,0 +1,1 @@
+import{Q as a}from"./QPage-BMOPsnQn.js";import{_ as s,c as t,o,w as r,a8 as c}from"./index-DqIGX8Uj.js";const p={name:"ReportesActualesPage"};function n(f,e,_,d,l,m){return o(),t(a,{class:"q-pa-md"},{default:r(()=>e[0]||(e[0]=[c(" asasa ")])),_:1})}const x=s(p,[["render",n]]);export{x as default};
